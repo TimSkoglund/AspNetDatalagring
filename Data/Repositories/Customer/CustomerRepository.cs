5583d75d26851982c585a647456027598677259c
Tim Skoglund <Timskoglund@hotmail.com>
2025-05-04 19:44:33 +0200

    Ny för customer

---
+++ Data/Repositories/Customer/CustomerRepository.cs
@@ -0,0 +1,9 @@
+﻿using Data.Context;
+using Data.Entities;
+using Data.Repositories.Base;
+
+namespace Data.Repositories.Customer;
+
+public class CustomerRepository(DataContext ctx)
+    : BaseRepository<CustomerEntity>(ctx), ICustomerRepository
+{ }
---
+++ Data/Repositories/Customer/ICustomerRepository.cs
@@ -0,0 +1,8 @@
+﻿using Data.Entities;
+using Data.Repositories.Base;
+
+namespace Data.Repositories.Customer
+{
+    public interface ICustomerRepository : IBaseRepository<CustomerEntity>
+    { }
+}
