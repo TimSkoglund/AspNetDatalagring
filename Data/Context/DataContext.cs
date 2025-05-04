8d9568ac37435707978496b7e7446c35cd93c676
Tim Skoglund <Timskoglund@hotmail.com>
2025-05-04 19:46:23 +0200

    Context

---
+++ Data/Context/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<CustomerEntity> Customers  => Set<CustomerEntity>();
+    public DbSet<ProductEntity>  Products   => Set<ProductEntity>();
+    public DbSet<StatusTypeEntity> StatusTypes => Set<StatusTypeEntity>();
+    public DbSet<UserEntity>    Users      => Set<UserEntity>();
+}
---
+++ Data/Context/DataContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Context;
+
+public class DataContextFactory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseSqlite("Data Source=project.db")
+            .Options;
+        return new DataContext(options);
+    }
+}
