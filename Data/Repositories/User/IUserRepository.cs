6b974ab8cf5ca7a7965fe3b0236f3370f026f1b9
Tim Skoglund <Timskoglund@hotmail.com>
2025-05-04 19:45:57 +0200

    ueser

---
+++ Data/Repositories/User/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using Data.Entities;
+using Data.Repositories.Base;
+
+namespace Data.Repositories.User
+{
+    public interface IUserRepository : IBaseRepository<UserEntity>
+    {
+        public Task<UserEntity?> GetUserAsync(string email, string passwordHash, CancellationToken cancellationToken);
+    }
+}
---
+++ Data/Repositories/User/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using Data.Context;
+using Data.Entities;
+using Data.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories.User;
+
+public class UserRepository(DataContext ctx)
+    : BaseRepository<UserEntity>(ctx), IUserRepository
+{
+    public async Task<UserEntity?> GetUserAsync(string email, string passwordHash, CancellationToken cancellationToken)
+    {
+        return await Db.FirstOrDefaultAsync(
+            u => u.Email == email && u.Password == passwordHash,
+            cancellationToken);
+    }
+}
