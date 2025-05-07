c953b8fb09215524a5555b749773b78dbc8572f4
timskoglund <timskoglund@hotmail.com>
2025-05-07 16:03:19 +0200

    Add project files.

---
+++ Data/Entities/StatusEntety.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Data.Entities;
+
+[Index(nameof(StatusName), IsUnique = true)]
+public class StatusEntety
+{
+    [Key]
+    public int Id { get; set; }
+    public string StatusName { get; set; } = null!;
+    
+    public virtual ICollection<ProjectEntety> Projects { get; set; } = [];
+}
---
+++ Data/Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections;
+
+namespace Data.Entities;
+
+public class UserEntity : IdentityUser 
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? JobTitle { get; set; }
+
+    public virtual ICollection<ProjectEntety> Projects { get; set; } = [];
+}
