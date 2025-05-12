7d331f5491cd0c862d2bb9c0af4417a85caf4e15
timskoglund <timskoglund@hotmail.com>
2025-05-12 11:18:31 +0200

    Data lager klart

---
+++ Data/Repositories/ProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using Data.Contexts;
+using Data.Entities;
+using Domain.Models;
+
+namespace Data.Repositories;
+
+public interface IProjectRepository : IBaseRepository<ProjectEntety, Project>
+{
+}
+
+public class ProjectRepository(AppDbContext context) : BaseRepository<ProjectEntety, Project>(context), IProjectRepository
+{
+}
